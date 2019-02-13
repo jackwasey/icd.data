ae379d7614bc35b38e7f7f3f8a31673861769a19
Jack O. Wasey <jack@jackwasey.com>
2019-02-13 17:00:41 -0500

    include codes and description for France ICD-10-FR

--- R/cim-10-fr.R
+++ R/cim-10-fr.R
@@ -1,14 +1,17 @@
 #' Read the definitions of the French edition of ICD-10
 #' @template save_data
 #' @keywords internal
-parse_cim_fr <- function(save_data = TRUE) {
+parse_cim_fr <- function(save_data = FALSE) {
   cim_raw <- read.delim(
     bzfile(file.path("data-raw", "CIM-10-FR.txt.bz2"),
            encoding = "Latin1"),
     header = FALSE,
     sep = "|",
     as.is = TRUE)
-  cim <- cim_raw[c(1, 5, 6)]
-  names(cim) <- c("code", "desc_short", "desc_long")
-  cim[["code"]] <- trimws(cim[["icd10"]])
+  icd10fr2008 <- cim_raw[c(1, 5, 6)]
+  names(icd10fr2008) <- c("code", "desc_short", "desc_long")
+  icd10fr2008[["code"]] <- trimws(icd10fr2008[["code"]])
+  if (save_data)
+    save_in_data_dir(icd10fr2008)
+  invisible(icd10fr2008)
 }
