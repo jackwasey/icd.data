85ebc32901c5778f37b2f5c605cde47aec28bbf6
Jack O. Wasey <jack@jackwasey.com>
2019-03-22 16:59:14 -0400

    typo

--- R/bindings.R
+++ R/bindings.R
@@ -117,7 +117,7 @@ makeActiveBinding("icd10cm_latest", .icd10cm_latest_binding, environment())
 lockBinding("icd10cm_latest", environment())
 
 .icd9cm_billable_binding <- function(x) {
-  if (.verbose() && .interact()) {
+  if (.verbose() && .interactive()) {
     message("Use icd9cm_leaf_v32 instead of icd9cm_billable.")
   }
   icd9cm_billable <- list()
