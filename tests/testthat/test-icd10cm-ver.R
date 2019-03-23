92cdb425a3153a30c025aa3603204cb36888ec64
Jack O. Wasey <jack@jackwasey.com>
2019-03-23 18:58:08 -0400

    rejig a test to correctly set a temp option

--- tests/testthat/test-icd10cm-ver.R
+++ tests/testthat/test-icd10cm-ver.R
@@ -21,9 +21,9 @@ test_that("all available data is reported", {
 
 test_that("temporarily set active version", {
   skip_icd10cm_flat_avail("2014")
-  with_icd10cm_version("2014", code = {
-    expect_identical(nrow(icd.data::get_icd10cm_active()), nrow(icd10cm2014))
-  })
+  expect_identical(
+    with_icd10cm_version("2014", nrow(get_icd10cm_active())),
+    nrow(icd10cm2014))
 })
 
 test_that("basic Belgian", {
