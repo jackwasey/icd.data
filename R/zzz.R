36909a0e534402377293d92e52751f759b3855a4
Jack O. Wasey <jack@jackwasey.com>
2019-03-24 09:18:44 -0400

    correct global variable coverage

--- R/zzz.R
+++ R/zzz.R
@@ -41,6 +41,9 @@ utils::globalVariables(c(
   "icd9_sub_chapters",
   "icd9_chapters",
   "icd9_majors",
+  "icd10_sub_chapters",
+  "icd10_chapters",
+  "icd10cm2016",
   "icd10cm2019",
   "icd9cm_hierarchy"
 ))
